9e4e86fd7a7b6857e1332e29ccec4cfdc1a63bfc
CW-MD <cdayton96@gmail.com>
2021-11-23 17:43:56 -0500

    reverseInt Solution

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # AlgoPractice
 A Repository full of whiteboard-style questions, as well as unit tests done via Jest
 - [x] reverseString
+- [x] Palindrome
+- [x] reverseInt
+
---
+++ exercises/reverseint/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+    let stack = []
+    if(n <= 9 && n > 0){
+        return n
+    }
+    let reversed = parseInt(n.toString().split('').reverse().join(''))
+
+    if(n < 0){
+        return -Math.abs(reversed)
+    }
+    return reversed;
+}
+
+module.exports = reverseInt;
